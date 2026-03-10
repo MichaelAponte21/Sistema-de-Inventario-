3ac9b8f41fc4a01b68742b6825c0aac2004b8207
Michael Aponte <michaelapontero@gmail.com>
2026-03-10 15:00:22 -0500

    Inicializacion de spring

---
+++ SistemaInventario/src/main/java/EntornosProgramacion/SistemaInventario/SistemaInventarioApplication.java
@@ -0,0 +1,13 @@
+package EntornosProgramacion.SistemaInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInventarioApplication.class, args);
+	}
+
+}
